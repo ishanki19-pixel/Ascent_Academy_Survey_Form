8fe3c87484266c210937f76cce46c0ecf0094b89
ishanki19-pixel <81527206+ishanki19-pixel@users.noreply.github.com>
2023-04-25 15:29:59 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,17 +1,12 @@
 function validateForm() {
-	// Get values from form inputs
 	var name = document.getElementById("name").value;
 	var age = document.getElementById("age").value;
 	var gender = document.getElementById("gender").value;
 	var email = document.getElementById("email").value;
 	var phone = document.getElementById("phone").value;
 	var message = document.getElementById("message").value;
-
-	// Define regular expression patterns for email and phone
 	var emailPattern = /\S+@\S+\.\S+/;
 	var phonePattern = /^\d{10}$/;
-
-	// Define error messages
 	var errors = "";
 	if (name === "") {
 		errors += "Name is required.\n";
